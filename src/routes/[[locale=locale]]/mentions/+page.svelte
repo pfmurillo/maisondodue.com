13192fdba91d97223f77c2a1e007b0496e0288d4
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2024-02-22 17:40:18 +0100

    feat: add legal information page

---
+++ src/routes/[[locale=locale]]/mentions/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { _ } from '$lib/i18n';
+</script>
+
+<h1>{$_('mentions.h1')}</h1>
+<p>{@html $_('mentions.p1')}</p>
+<p>{@html $_('mentions.p2')}</p>
+<p>{@html $_('mentions.p3')}</p>
+<p>{@html $_('mentions.p4')}</p>
+<p>{@html $_('mentions.p5')}</p>
+<p>{@html $_('mentions.p6')}</p>
+<p>{@html $_('mentions.p7')}</p>
