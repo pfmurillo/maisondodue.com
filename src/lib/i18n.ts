ababf3c8f409fd360ec2ac5250c7da1289bbd3e4
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2024-11-04 23:52:50 +0100

    chore: paraglide configuration

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,3 +1,6 @@
 import * as runtime from '$lib/paraglide/runtime';
 import { createI18n } from '@inlang/paraglide-sveltekit';
-export const i18n = createI18n(runtime);
+export const i18n = createI18n(runtime, {
+	defaultLanguageTag: 'fr',
+	prefixDefaultLanguage: 'always'
+});
