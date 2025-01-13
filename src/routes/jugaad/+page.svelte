699037628045a69a8b1fce0634ce991043a97a25
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2025-01-13 16:19:43 +0100

    feat: better image compression

--- src/routes/jugaad/+page.svelte
+++ src/routes/jugaad/+page.svelte
@@ -8,18 +8,18 @@
 
 <div class="m-auto mb-14 mt-7 grid max-w-screen-lg grid-cols-2 gap-4 p-3 md:grid-cols-3">
 	<div
-		class="bg-moka-500 col-start-1 col-end-3 flex items-center rounded-3xl p-6 text-lg leading-8 text-white md:col-start-1 md:col-end-4 md:p-12"
+		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-moka-500 p-6 text-lg leading-8 text-white md:col-start-1 md:col-end-4 md:p-12"
 	>
 		{m.early_keen_gecko_pet()}
 	</div>
 	<div>
-		<Image src="/jugaad/69bed16d-d24a-441f-b0c8-fdf5374dae98.png" alt="" />
+		<Image src="/jugaad/01.webp" alt="" />
 	</div>
 	<div>
-		<Image src="/jugaad/760cb900-54fb-43b7-a3f4-cfd51a9b5583.png" alt="" />
+		<Image src="/jugaad/02.webp" alt="" />
 	</div>
 	<div>
-		<Image src="/jugaad/b0b47710-4fe2-496e-8776-58cf162c28e9.png" alt="" />
+		<Image src="/jugaad/03.webp" alt="" />
 	</div>
 	<div
 		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-spinach-300 p-6 text-lg leading-8 text-white md:p-12"
@@ -27,10 +27,10 @@
 		{m.gaudy_less_cod_bloom()}
 	</div>
 	<div>
-		<Image src="/jugaad/IMG_20230605_215714.jpg" alt="" />
+		<Image src="/jugaad/04.webp" alt="" />
 	</div>
 	<div>
-		<Image src="/jugaad/IMG_20240523_105511.jpg" alt="" />
+		<Image src="/jugaad/05.webp" alt="" />
 	</div>
 	<div
 		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-cassis-500 p-6 text-lg leading-8 text-white md:col-start-2 md:col-end-4 md:p-12"
@@ -38,14 +38,14 @@
 		{m.lazy_raw_squid_dial()}
 	</div>
 	<div
-		class="bg-curry-300 col-start-1 col-end-3 row-start-8 row-end-9 flex items-center rounded-3xl p-6 text-lg leading-8 text-white md:col-start-1 md:col-end-2 md:row-start-5 md:row-end-6 md:p-12"
+		class="col-start-1 col-end-3 row-start-8 row-end-9 flex items-center rounded-3xl bg-curry-300 p-6 text-lg leading-8 text-white md:col-start-1 md:col-end-2 md:row-start-5 md:row-end-6 md:p-12"
 	>
 		{m.icy_fun_rook_rush()}
 	</div>
 	<div>
-		<Image src="/jugaad/MaisonDodue_Jugaad.png" alt="" />
+		<Image src="/jugaad/06.webp" alt="" />
 	</div>
 	<div>
-		<Image src="/jugaad/MaisonDodue_Jugaad_Header.png" alt="" />
+		<Image src="/jugaad/07.webp" alt="" />
 	</div>
 </div>
