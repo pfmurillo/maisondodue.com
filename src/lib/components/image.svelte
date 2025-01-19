6695d0ca5c7c9d8c332dbebe5d84a6c094c6aa88
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2025-01-19 21:35:49 +0100

    feat: remove vintage filter on images

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -2,8 +2,9 @@
 	const { src, alt }: { src: string; alt: string } = $props();
 </script>
 
-<div
+<!-- <div
 	class="relative aspect-square after:absolute after:top-0 after:h-full after:w-full after:rounded-3xl after:bg-vintage"
->
+> -->
+<div class="relative aspect-square">
 	<img class="absolute aspect-square h-full w-full rounded-3xl object-cover" {src} {alt} />
 </div>
