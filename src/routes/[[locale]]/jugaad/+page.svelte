70461b6b9298fea6a986fcd8ec8478e033186d4e
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2025-02-12 14:03:09 +0100

    feat: new picture to the jugaad section

--- src/routes/[[locale]]/jugaad/+page.svelte
+++ src/routes/[[locale]]/jugaad/+page.svelte
@@ -8,9 +8,23 @@
 
 <div class="m-auto mb-14 mt-7 grid max-w-screen-lg grid-cols-2 gap-4 p-3 md:grid-cols-3">
 	<div
-		class="bg-almond-500 col-start-1 col-end-3 flex items-center rounded-3xl p-6 leading-8 text-black md:col-start-1 md:col-end-4 md:p-12"
+		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-almond-500 p-6 leading-8 text-black md:col-start-1 md:col-end-4 md:p-12"
 	>
 		{m.early_keen_gecko_pet()}
+	</div>
+	<div>
+		<Image src="/jugaad/08.webp" alt="" />
+	</div>
+	<div>
+		<Image src="/jugaad/05.webp" alt="" />
+	</div>
+	<div>
+		<Image src="/jugaad/03.webp" alt="" />
+	</div>
+	<div
+		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-almond-500 p-6 leading-8 text-black md:p-12"
+	>
+		{m.gaudy_less_cod_bloom()}
 	</div>
 	<div>
 		<Image src="/jugaad/01.webp" alt="" />
@@ -19,18 +33,7 @@
 		<Image src="/jugaad/02.webp" alt="" />
 	</div>
 	<div>
-		<Image src="/jugaad/03.webp" alt="" />
-	</div>
-	<div
-		class="bg-almond-500 col-start-1 col-end-3 flex items-center rounded-3xl p-6 leading-8 text-black md:p-12"
-	>
-		{m.gaudy_less_cod_bloom()}
-	</div>
-	<div>
 		<Image src="/jugaad/04.webp" alt="" />
-	</div>
-	<div>
-		<Image src="/jugaad/05.webp" alt="" />
 	</div>
 	<!-- <div
 		class="col-start-1 col-end-3 flex items-center rounded-3xl bg-cassis-500 p-6 leading-8 text-white md:col-start-2 md:col-end-4 md:p-12"
@@ -38,7 +41,7 @@
 		{m.lazy_raw_squid_dial()}
 	</div> -->
 	<div
-		class="bg-almond-500 col-start-1 col-end-3 row-start-7 row-end-8 flex items-center rounded-3xl p-6 leading-8 text-black md:col-start-1 md:col-end-2 md:row-start-5 md:row-end-6 md:p-12"
+		class="col-start-1 col-end-3 row-start-7 row-end-8 flex items-center rounded-3xl bg-almond-500 p-6 leading-8 text-black md:col-start-2 md:col-end-3 md:row-start-5 md:row-end-6 md:p-12"
 	>
 		{m.icy_fun_rook_rush()}
 	</div>
