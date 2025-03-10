9c75061a7e7c2baa8b797deb7dae6e60ed9027f1
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2025-03-10 13:43:25 +0100

    fix: use reroute hook

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,7 +2,9 @@ import * as runtime from '$lib/paraglide/runtime';
 import { createI18n } from '@inlang/paraglide-sveltekit';
 export const i18n = createI18n(runtime, {
 	defaultLanguageTag: 'fr',
-	prefixDefaultLanguage: 'always',
+	//prefixDefaultLanguage: 'always',
+
+	// DO NOT USE TRANSLATED PATHNAME BECAUSE OF VERCEL ISSUE
 	pathnames: {
 		'/sieges': {
 			fr: '/sieges',
